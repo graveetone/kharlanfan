a566c5727b3f1d619788168783c1162968801eaf
qubeet25 <zastavskijs@gmail.com>
2022-04-21 04:54:54 +0300

    Finished adaptivity

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-let menu_button = document.getElementsByClassName("menu-button")[0];
-let menu = document.getElementsByClassName("flex-container")[0].getElementsByTagName("li");
+let menu_elems = document.querySelector(".menu-elems img");
+let nav = document.querySelector('nav');
+let IsMenuOpened = false;
+let animations = ["left-right-slider", "right-left-slider", "top-bottom-slider"];
 
-function displayLi(switcher) {
-    for (const li of document.querySelectorAll('.flex-container li')) {
-        li.style.display = switcher ? 'flex' : 'none';
-    }
-    // arrow_icon.classList.add("fa-arrow-turn-right");
-    // arrow_icon.classList.remove("fa-arrow-turn-down");
+function RandomChoise(array) {
+    let index = Math.floor(Math.random() * array.length);
+    return array[index];
 }
-menu_button.addEventListener('click', () => { displayLi(true) });
-document.querySelector(".logo").addEventListener('click', () => { displayLi(false) });
+
+function display() {
+    IsMenuOpened = !IsMenuOpened; // show if closed, close if shown
+    // show all menu items
+    for (const li of document.querySelectorAll('.flex-container li')) {
+        li.style.display = IsMenuOpened ? 'flex' : 'none';
+    }
+
+    nav.style = `animation: 0.5s ease-out ${RandomChoise(animations)};`;
+    let isvisible = IsMenuOpened ? "hidden" : 'visible';
+    document.querySelector(".menu-elems h4").style = `visibility: ${isvisible}`;
+    document.querySelector(".menu-elems").style.width = IsMenuOpened ? '40px' : "65%";
+}
+
+
+menu_elems.addEventListener('click', () => { display() });
